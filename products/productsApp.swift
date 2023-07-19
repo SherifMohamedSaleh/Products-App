8ae25694f2778615257b2d3c5422320367ba28da
Sherif Abd El-Moniem <sherif.abdelmoniem@devopsolution.net>
2023-07-19 21:39:59 +0300

    Initial Commit

---
+++ products/productsApp.swift
@@ -0,0 +1,17 @@
+//
+//  productsApp.swift
+//  products
+//
+//  Created by Sherif Abd El-Moniem on 19/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct productsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
